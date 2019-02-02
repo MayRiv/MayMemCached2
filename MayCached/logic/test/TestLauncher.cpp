c1920d736b3b9d73c27080acb4e61c6447940e94
Ihor Maidanovskyi <MayRiv@i.ua>
2019-02-02 16:06:40 +0200

    Created serializer to raw binary buffer

--- MayCached/logic/test/TestLauncher.cpp
+++ MayCached/logic/test/TestLauncher.cpp
@@ -1,6 +1,7 @@
 #include "gtest/gtest.h"
 //#include "TimeExpirationManagerTest.hpp"
 #include "TimeExpirationCoreTest.hpp"
+#include "DatabaseMarshallerTest.hpp"
 int main(int argc, char** argv)
 {
     testing::InitGoogleTest(&argc, argv);
